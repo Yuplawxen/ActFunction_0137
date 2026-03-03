baafd9682f14c75098cd8d0df100a67f1b81adb1
Yuplawxen <yuftiyannaaqurrota@gmail.com>
2026-03-03 20:24:03 +0700

    Implementasi Prosedur dan Fungsi

--- Praktikum 2.1.cpp
+++ Praktikum 2.1.cpp
@@ -5,3 +5,11 @@ using namespace std;
 // deklarasi global
 int p, l, luas;
 
+// Implementasi Prosedur dan Fungsi
+void input() {
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
+
